838b6a4ca51b5d30ecbce6d663b67f197a92008e
diego <diegoriberarodriguez@gmail.com>
2025-12-03 18:46:29 -0400

    Subiendo Sistema RentaCar otra vez

--- VercelApp/wsgi.py
+++ VercelApp/wsgi.py
@@ -7,12 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
-
-# Añade el directorio del proyecto al path
+# AÑADIR ESTO - MUY IMPORTANTE
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_dir)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VercelApp.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
