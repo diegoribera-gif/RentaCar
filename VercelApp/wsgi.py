37aeaed8827f02dc3aa02d29dfd14962a82d3912
diego <diegoriberarodriguez@gmail.com>
2025-12-03 18:57:58 -0400

    Mensaje  de los cambios nuevos

--- VercelApp/wsgi.py
+++ VercelApp/wsgi.py
@@ -1,21 +1,14 @@
-"""
-WSGI config for VercelApp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
-
 import os
 import sys
 
-# AÑADIR ESTO - MUY IMPORTANTE
-project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.insert(0, project_dir)
+# Añadir el directorio del proyecto al path
+project_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VercelApp.settings')
 
 from django.core.wsgi import get_wsgi_application
+
+# ⚠️ IMPORTANTE: Vercel necesita 'app' o 'handler'
 application = get_wsgi_application()
+app = application  # ← AÑADE ESTA LÍNEA
